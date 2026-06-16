f11250e21181d7232c561b75cfcc45466fba9198
markongaro <ongaromark8@gmail.com>
2026-06-16 05:26:55 -0400

    added function to convert bitstring to set chars

--- ch02/ex1.py
+++ ch02/ex1.py
@@ -4,7 +4,6 @@ n = len(u)
 A_set = {'a','c', 'e', 'f', 'h', 'd'}
 B_set = {'g', 'd', 'a', 'c'}
 
-
 def to_bitstring(subset, universe):
     bitmask = 0
     for i, item in enumerate(universe):
@@ -12,15 +11,22 @@ def to_bitstring(subset, universe):
             bitmask |= 1 << (len(universe) -1 - i)
     return bitmask
 
-
 def fmt(bitmask, length):
     return f"{bitmask:0{length}b}"
 
-# TOD0: Write a function that converts an integer bitmask to characters
+def to_set(bitmask, universe):
+    dump_set = set()
+    for i, item in enumerate(universe):
+        shift_len = len(universe) - 1 - i
+
+        if (bitmask >> shift_len) & 1:
+            dump_set.add(item)
+    return dump_set
 
 a_bits = to_bitstring(A_set, u)
 b_bits = to_bitstring(B_set, u)
 
+# TODO: Perform logic operations (Union, Intersection, Complement e.t.c)
 
 
 
